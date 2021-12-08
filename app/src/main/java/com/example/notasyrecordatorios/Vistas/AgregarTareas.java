be18366775a7720b4309eea70647121c898fd92b
Daniel García Paniagua <danielgarcia488.dg@gmail.com>
2021-12-07 22:25:25 -0600

    Notificaciones terminado

--- app/src/main/java/com/example/notasyrecordatorios/Vistas/AgregarTareas.java
+++ app/src/main/java/com/example/notasyrecordatorios/Vistas/AgregarTareas.java
@@ -1,12 +1,22 @@
 package com.example.notasyrecordatorios.Vistas;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +26,7 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.example.notasyrecordatorios.AlarmReceiver;
 import com.example.notasyrecordatorios.Database.DatabaseTareas;
 import com.example.notasyrecordatorios.R;
 
@@ -24,20 +35,33 @@ import java.util.Calendar;
 
 
 public class AgregarTareas extends AppCompatActivity {
+
     EditText titulo, descripcion;
     Button btnAgregar;
     TextView time;
     Button btnTime;
     Button btnDate;
     int currHour, currMinute, currSecond,currDay, currMonth, currYear;
+    int selectedHour, selectedMinute, selectedSecond, selectedDay, selectedMonth, selectedYear;
 
     String tiempo="";
     String fecha="";
 
+    private PendingIntent pendingIntent;
+
+    private AlarmManager alarmManager;
+
+    private Calendar calendarAlarm;
+
+    private static final String CHANNEL_ID = "RECORDATORIOS-TAREAS";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_agregar_tareas);
+
+        createNotificationChannel();
 
         titulo = findViewById(R.id.titulo);
         descripcion = findViewById(R.id.descripcion);
@@ -64,10 +88,19 @@ public class AgregarTareas extends AppCompatActivity {
                     public void onTimeSet(TimePicker timePicker, int i, int i1) {
                         tiempo=new DecimalFormat("00").format(i)+":"+new DecimalFormat("00").format(i1)+":00";
                         time.setText(tiempo);
+                        selectedHour=i;
+                        selectedMinute=i1;
+                        selectedSecond=0;
 
+                        calendarAlarm = Calendar.getInstance();
+                        calendarAlarm.set(Calendar.HOUR_OF_DAY, i);
+                        calendarAlarm.set(Calendar.MINUTE, i1);
+                        calendarAlarm.set(Calendar.SECOND, 0);
+                        calendarAlarm.set(Calendar.MILLISECOND, 0);
                     }
                 }, currHour, currMinute, false);
                 dialog.show();
+
             }
         });
 
@@ -79,6 +112,10 @@ public class AgregarTareas extends AppCompatActivity {
                     public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
                         fecha=i+"-"+(new DecimalFormat("00").format(i1+1))+"-"+new DecimalFormat("00").format(i2)+" ";
                         time.setText(fecha + tiempo);
+                        selectedYear=i;
+                        selectedMonth=i1;
+                        selectedDay=i2;
+
 
                     }
                 }, currYear,currMonth,currDay);
@@ -87,12 +124,16 @@ public class AgregarTareas extends AppCompatActivity {
         });
 
         btnAgregar.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
                 if (!TextUtils.isEmpty(titulo.getText().toString()) && !TextUtils.isEmpty(descripcion.getText().toString())){
                     DatabaseTareas db = new DatabaseTareas(AgregarTareas.this);
                     System.out.println(time.getText().toString());
                     db.agregarTarea(titulo.getText().toString(),descripcion.getText().toString(),time.getText().toString());
+
+                    setAlarm();
+
 
                     Intent intent = new Intent(AgregarTareas.this, MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
@@ -105,4 +146,59 @@ public class AgregarTareas extends AppCompatActivity {
             }
         });
     }
+
+    private void setAlarm() {
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        Intent intent = new Intent(this,AlarmReceiver.class);
+
+        pendingIntent = PendingIntent.getBroadcast(this,0,intent,0);
+
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,calendarAlarm.getTimeInMillis(),
+                AlarmManager.INTERVAL_DAY,pendingIntent);
+
+        Toast.makeText(this, "Alarm set Successfully", Toast.LENGTH_SHORT).show();
+
+
+
+    }
+
+
+    private void mostrarNotificacion(Context context, Intent intent) {
+        //createNotificationChannel(context,intent);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Titulo recordatorio")
+                .setContentText("Te recuerdo tarea pendiente")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(context);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(1001, builder.build());
+
+    }
+
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            CharSequence name = "foxandroidReminderChannel";
+            String description = "Channel For Alarm Manager";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel("foxandroid",name,importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+
+        }
+
+
+    }
+
+
+
 }
